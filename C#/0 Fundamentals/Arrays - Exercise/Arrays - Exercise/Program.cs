5a60846bf7e2a7f796da54ba4fd304e2d01614ff
technick9000 <73235434+technick9000@users.noreply.github.com>
2021-06-05 22:00:17 +0300

    [ Future proofing ] Created a Seperate branch for C#

---
+++ C#/0 Fundamentals/Arrays - Exercise/Arrays - Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _01Train();
+        }
+
+        private static void _01Train()
+        {
+            int n = int.TryParse(Console.ReadLine(), out n) ? n : 1;
+            int[] wagons = { };
+
+            for (int i = 0; i < n; i++)
+            {
+                int passangers = int.TryParse(Console.ReadLine(), out passangers) ? passangers : 0;
+                Array.Resize(ref wagons, wagons.Length + 1);
+                wagons[wagons.GetUpperBound(0)] = passangers;
+            }
+
+            Console.WriteLine(String.Join(' ', wagons));
+            Console.WriteLine(wagons.Sum());
+        }
+    }
+}
